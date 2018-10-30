9931049854f0cca45f78f1190750347009915fc4
Sylvain Rabot <s.rabot@lectra.com>
2018-10-30 15:04:18 +0100

    Workaround Azure/azure-sdk-for-go/issues/3171
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- vendor/github.com/Azure/azure-sdk-for-go/services/batch/mgmt/2017-09-01/batch/models.go
+++ vendor/github.com/Azure/azure-sdk-for-go/services/batch/mgmt/2017-09-01/batch/models.go
@@ -2031,8 +2031,8 @@ type ResizeOperationStatus struct {
 	// ResizeTimeout - The default value is 15 minutes. The minimum value is 5 minutes. If you specify a value less than 5 minutes, the Batch service returns an error; if you are calling the REST API directly, the HTTP status code is 400 (Bad Request).
 	ResizeTimeout *string `json:"resizeTimeout,omitempty"`
 	// NodeDeallocationOption - The default value is requeue. Possible values include: 'Requeue', 'Terminate', 'TaskCompletion', 'RetainedData'
-	NodeDeallocationOption ComputeNodeDeallocationOption `json:"nodeDeallocationOption,omitempty"`
-	StartTime              *date.Time                    `json:"startTime,omitempty"`
+	NodeDeallocationOption interface{} `json:"nodeDeallocationOption,omitempty"`
+	StartTime              *date.Time  `json:"startTime,omitempty"`
 	// Errors - This property is set only if an error occurred during the last pool resize, and only when the pool allocationState is Steady.
 	Errors *[]ResizeError `json:"errors,omitempty"`
 }
