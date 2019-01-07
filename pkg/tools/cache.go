d115a6af0629193cd064e00cf5d6aefe888ac7dc
Sylvain Rabot <s.rabot@lectra.com>
2019-01-07 13:34:51 +0100

    Add mutex to GetCache
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- pkg/tools/cache.go
+++ pkg/tools/cache.go
@@ -1,17 +1,22 @@
 package tools
 
 import (
+	"sync"
 	"time"
 
 	gocache "github.com/patrickmn/go-cache"
 )
 
 var (
+	mutex  = sync.RWMutex{}
 	caches = make(map[time.Duration]*gocache.Cache)
 )
 
-// GetCache get a caching object
+// GetCache get a cache object
 func GetCache(duration time.Duration) *gocache.Cache {
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	if _, ok := caches[duration]; !ok {
 		caches[duration] = gocache.New(duration, 2*time.Minute)
 	}
